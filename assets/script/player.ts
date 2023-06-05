a90755057b6bf2c6952b89e5da1af9e36316f348
110062320 <easylin0126@gmail.com>
2023-06-05 23:38:45 +0800

    basic player control and 2.5D collider

---
+++ assets/script/player.ts
@@ -0,0 +1,125 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+    left_move:boolean=false;
+    right_move:boolean=false;
+    jump:boolean=false;
+    on_ground:boolean=false;
+
+    playerSpeed:number=0;
+
+    private anim=null;
+
+    onLoad () {
+        cc.director.getPhysicsManager().enabled = true;
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);    
+        this.anim=this.getComponent(cc.Animation);
+    }
+    onKeyDown(event) {
+        if(this.node.name=='player1'){
+            if(event.keyCode == cc.macro.KEY.up&&this.on_ground==true){//w jump
+                this.jump=true;
+                this.on_ground=false;
+            }
+            else if(event.keyCode==cc.macro.KEY.left){//left
+                this.right_move=false;
+                this.left_move=true;
+            }
+            else if(event.keyCode==cc.macro.KEY.right){//right
+                this.right_move=true;
+                this.left_move=false;
+            }
+        }
+        else{
+            if(event.keyCode == cc.macro.KEY.w&&this.on_ground==true){//w jump
+                this.jump=true;
+                this.on_ground=false;
+            }
+            else if(event.keyCode==cc.macro.KEY.a){//left
+                this.right_move=false;
+                this.left_move=true;
+            }
+            else if(event.keyCode==cc.macro.KEY.d){//right
+                this.right_move=true;
+                this.left_move=false;
+            }
+        }
+        
+    }
+    onKeyUp(event){
+        if(this.node.name=='player1'){
+            if(event.keyCode == cc.macro.KEY.up){//w jump
+                this.jump=false;
+            }
+            else if(event.keyCode==cc.macro.KEY.left){//left
+                this.left_move=false;
+                this.anim.stop("player_1_walk");
+            }
+            else if(event.keyCode==cc.macro.KEY.right){//right
+                this.right_move=false;
+                this.anim.stop("player_1_walk");
+            }
+        }
+        else{
+            if(event.keyCode == cc.macro.KEY.w){//w jump
+                this.jump=false;
+            }
+            else if(event.keyCode==cc.macro.KEY.a){//left
+                this.left_move=false;
+                this.anim.stop("player_1_walk");
+            }
+            else if(event.keyCode==cc.macro.KEY.d){//right
+                this.right_move=false;
+                this.anim.stop("player_1_walk");
+            }
+        }
+        
+    }
+    start () {
+
+    }
+    onBeginContact(contact,self,other){
+        if(other.node.name=='Platform1'||other.node.name=='Platform2'||other.node.name=='Platform3')
+            this.on_ground=true;
+    }
+    update (dt) {
+        this.playerSpeed=0;
+        if(this.left_move) {
+            this.playerSpeed=-400;
+            this.node.scaleX=-0.1;  // modify node's X scale value to change facing direction
+            if(!this.anim.getAnimationState('player_1_walk').isPlaying){
+                this.anim.play("player_1_walk");
+            }
+            
+        } 
+        else if(this.right_move) {
+            this.playerSpeed=400;
+            this.node.scaleX=0.1;  
+            if(!this.anim.getAnimationState('player_1_walk').isPlaying){
+                this.anim.play("player_1_walk");
+            }
+        }
+        if(this.jump){
+            this.getComponent(cc.RigidBody).linearVelocity=cc.v2(0,650);
+            this.jump=false;
+        }
+        this.node.x += this.playerSpeed *dt; 
+    }
+}
