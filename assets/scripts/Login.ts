6cdef934a7f004fb01fc3823dba5bfc7cb3bea36
110062105 <stanlai.0411@gmail.com>
2023-06-15 12:59:30 +0800

    update

--- assets/scripts/Login.ts
+++ assets/scripts/Login.ts
@@ -9,7 +9,7 @@ export default class NewClass extends cc.Component {
     Login():void{
         firebase.auth().signInWithEmailAndPassword(this.inputemail, this.inputpassword)
         .then((userCredential) => {
-            cc.director.loadScene("Logincheck");
+            cc.director.loadScene("Login-check");
         })
         .catch(e => {
             alert('Invalid Email or Password');
@@ -19,7 +19,7 @@ export default class NewClass extends cc.Component {
     NewAccount():void{
         firebase.auth().createUserWithEmailAndPassword(this.inputemail, this.inputpassword)
         .then((userCredential) => {
-            cc.director.loadScene("Logincheck");
+            cc.director.loadScene("Login-check");
         })
         .catch(e => {
             alert('Invalid Email or Password');
