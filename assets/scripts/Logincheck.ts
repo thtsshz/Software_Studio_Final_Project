6aedde32940f957fc2dc37dd61ce61023e94e48f
110062105 <stanlai.0411@gmail.com>
2023-06-16 23:06:33 +0800

    add multipler test fast 1

--- assets/scripts/Logincheck.ts
+++ assets/scripts/Logincheck.ts
@@ -5,7 +5,25 @@ export default class NewClass extends cc.Component {
 
     protected onLoad(): void {
         const user = firebase.auth().currentUser;
-        cc.find("Canvas/Email").getComponent(cc.Label).string = user.email;
+        cc.find("Canvas/Email").getComponent(cc.Label).string = user.uid;
+
+        
+        const data = {
+            userID: user.uid
+        }
+        const request = fetch('http://192.168.50.62:8080/createroom', {
+            method: "POST",
+            body: JSON.stringify(data),
+        }).then(res => {
+            console.log("", res)
+            return res.json()
+        }).catch(err => {
+            console.log(err);
+        }).then(res => {
+            console.log("onload check: ", res);
+            // document.getElementById('response').innerHTML = JSON.stringify(res);
+        })
+
     }
     Logout():void{
         firebase.auth().signOut()
@@ -20,13 +38,16 @@ export default class NewClass extends cc.Component {
     multiplayertest():void{
         cc.director.loadScene("multiplayer-test");
     }
+    multiplayertestfast():void{
+        cc.director.loadScene("multiplayer-test-fast");
+    }
     start () {
-        // console.log('start : ');
-        // fetch("http://192.168.50.62:8080/ping", {mode : "no-cors"}).then((response: Response) => {
-        //     return response.text()
-        // }).then((value) => {
-        //     console.log("res : "value);
-        // })
+        console.log('start : ');
+        fetch("http://192.168.50.62:8080/ping").then((response: Response) => {
+            return response.text()
+        }).then((value) => {
+            console.log("res : ", value);
+        })
     }
     update (dt) {
         // const promise = new Promise((res, rej) => {
