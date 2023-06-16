5c91db1e151496f190c861c6e34891d1ba836f8e
110062320 <easylin0126@gmail.com>
2023-06-16 21:49:19 +0800

    Liu fifth

---
+++ assets/unity/BasicAttack/skill.py
@@ -0,0 +1,27 @@
+import cv2        
+from PIL import Image
+videoFile = 'BasicAttack.mp4'
+outputFile = 'image/'
+vc = cv2.VideoCapture(videoFile)
+c = 1
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    print('openerror!')
+    rval = False
+
+timeF = 5  #視頻幀計數間隔次數
+while rval:
+    print(1)
+    #print(c)
+    rval, frame = vc.read()
+    if c % timeF == 0:
+        print(2)
+        cv2.imwrite(outputFile + str(int(c / timeF)) + '.jpg', frame)
+
+        im1 = Image.open(outputFile + str(int(c / timeF)) + '.jpg')
+        im1.save(outputFile + str(int(c / timeF)) + '.png')
+        
+    c += 1
+    cv2.waitKey(1)
+vc.release()
