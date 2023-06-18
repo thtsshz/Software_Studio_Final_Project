1d22eb31b6fd0257111ca080abee0610dd0dbb10
110062320 <easylin0126@gmail.com>
2023-06-18 15:45:11 +0800

    camera movement in scene2

--- assets/scripts/camera2.ts
+++ assets/scripts/camera2.ts
@@ -19,10 +19,30 @@ export default class NewClass extends cc.Component {
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
-
+    cnt=0;
+    finish=0;
     start () {
-
+        
     }
 
-    // update (dt) {}
+    update (dt) {
+        if(this.getComponent(cc.Camera).zoomRatio>2)  
+            this.getComponent(cc.Camera).zoomRatio-=0.04;
+        else{
+            if(this.cnt==0){
+                let action=cc.sequence(cc.moveBy(1,-395,-190).easing(cc.easeCircleActionOut()),cc.moveBy(1,905,365).easing(cc.easeCircleActionOut()),cc.moveBy(1,-510,-175).easing(cc.easeCircleActionOut()));
+                this.node.runAction(action);
+                this.cnt++;
+                this.scheduleOnce(function(){
+                    this.finish=1;
+                },2);
+            }
+        }
+        if(this.finish){
+            this.getComponent(cc.Camera).zoomRatio=Math.max(this.getComponent(cc.Camera).zoomRatio-0.01,1);
+            if(this.getComponent(cc.Camera).zoomRatio<=1)
+                this.finish=0;
+        
+        }
+    }
 }
