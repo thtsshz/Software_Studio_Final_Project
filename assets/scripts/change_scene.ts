af179e82db262f6e66015e41185ed1fbd3701627
110062105 <stanlai.0411@gmail.com>
2023-06-18 20:29:15 +0800

    update

--- assets/scripts/change_scene.ts
+++ assets/scripts/change_scene.ts
@@ -1,33 +1,22 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
+export default class change_scene extends cc.Component {
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        
+    }
 
     start () {
         this.getComponent(cc.VideoPlayer).play();
         this.scheduleOnce(function(){
-                    cc.director.loadScene("Lobby");
-
+                cc.director.loadScene("Lobby");
         },14);
 
     }
+    update (dt) {
 
-    // update (dt) {}
+    }
 }
